0715e907c89e726d7cd4849971ecac7f51ca6e0c
davidleclerc <47993835+davidleclerc@users.noreply.github.com>
2020-04-18 13:47:13 -0500

    added edit user fields on settings page

--- IMS/FrontEnd/src/app/Components/settings/settings.component.ts
+++ IMS/FrontEnd/src/app/Components/settings/settings.component.ts
@@ -6,6 +6,7 @@ export interface Employee{
   password:string;
   level: number;
   id:number;
+  show:boolean;
   
 }
 
@@ -16,20 +17,28 @@ export interface Employee{
 })
 export class SettingsComponent implements OnInit {
 
+  //todo: save updated employee to database
+  updateEmployee(username:string,password:string,level:number ){
+  }
+  
+  //todo: get employee data from database
+  
   // test data
   users: Employee[] =[
     {
       username: 'employee1',
       password: 'password',
       level: 1,
-      id:1
+      id:1,
+      show:false
       
     },
     {
       username: 'employee2',
       password: 'password',
       level: 2,
-      id:2
+      id:2,
+      show:false
       
     }
   ]
