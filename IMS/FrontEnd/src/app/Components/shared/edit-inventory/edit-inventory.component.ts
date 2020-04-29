7dcf62b1bda69f1006743efef22aebaf7b460063
Nick <ns0913569@otc.edu>
2020-04-29 13:17:00 -0500

    Sorry had one more small bug to fix, ignore any errors pertaining to any of the variables missing in the html file

--- IMS/FrontEnd/src/app/Components/shared/edit-inventory/edit-inventory.component.ts
+++ IMS/FrontEnd/src/app/Components/shared/edit-inventory/edit-inventory.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { BuySellService } from 'src/app/Services/Buy-Sell/buy-sell.service';
+import { InventoryItem } from 'src/app/Interfaces/Inventory-Items/inventory-item';
 
 @Component({
   selector: 'app-edit-inventory',
@@ -10,7 +11,7 @@ import { BuySellService } from 'src/app/Services/Buy-Sell/buy-sell.service';
 export class EditInventoryComponent implements OnInit {
 
   id: string;
-  product: Object;
+  product: InventoryItem;
 
   constructor(private search: BuySellService, public dialogRef: MatDialogRef<EditInventoryComponent>, @Inject(MAT_DIALOG_DATA) data) { 
     this.id = data.id;
@@ -27,8 +28,9 @@ export class EditInventoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.search.getProduct(this.id).subscribe((result: object) =>{
-      console.log(result);
-      this.product = result;
+      
+      this.product = {id: result[0]['productID'], name: result[0]['name'], description: result[0]['description'], price : Number(result[0]['price']), used: Number(result[0]['used']), stock: Number(result[0]['stock'])};
+      console.log(this.product);
     });
   }
 
